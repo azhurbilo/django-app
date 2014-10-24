77617433348fe3f74ac7fb8ac82dcd654c2adef1
Artem Zhurbilo <artem.zhurbilo@itechart-group.com>
2014-10-24 17:36:18 +0300

    add 3 build step to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-# Run the role/playbook with ansible-playbook.
+# Step 1:
+echo ">>> Step 1: check role/playbook syntax"
 ansible-playbook -i tests/inventory tests/test.yml --syntax-check
+
+# Step 2:
+echo ">>> Step 2: run the role/playbook with ansible-playbook"
+ansible-playbook -i tests/inventory tests/test.yml
+
+# Step 3:
+echo ">>> Step 2: run the role/playbook again, checking to make sure it's idempotent."
+ansible-playbook -i tests/inventory tests/test.yml  | grep -q 'changed=0.*failed=0'\
+&& (echo 'Idempotence test: Success' && exit 0) || (echo 'Idempotence test: Fail' && exit 1)
